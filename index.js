832ce599560a5cf39a5c67506a14171d22e03499
David Björklund <david.bjorklund@gmail.com>
2017-02-17 12:26:55 +0100

    allow multiple inputs, use deglob to avoid .gitignore

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 /* @flow */
 const { readFileSync, writeFileSync } = require('fs');
-const glob = require('glob');
+const deglob = require('deglob');
 const { join } = require('path');
 
-const filenames = glob.sync(join(process.cwd(), process.argv[2]));
-
-filenames.forEach(filename => {
-  const file = readFileSync(filename, 'utf8');
-
-  if (file.includes('@flow')) {
-    return;
+deglob(process.argv.slice(2), (err, filenames) => {
+  if (err) {
+    throw err;
   }
 
-  const changedFile = file
-    .replace(/PropTypes\.object\.isRequired/g, 'Object')
-    .replace(/: PropTypes\.object\.isRequired/g, '?: Object')
-    .replace(/PropTypes\.string\.isRequired/g, 'string')
-    .replace(/: PropTypes\.string\.isRequired/g, '?: string')
-    .replace(/PropTypes\.bool\.isRequired/g, 'boolean')
-    .replace(/: PropTypes\.bool\.isRequired/g, '?: boolean')
-    .replace(/PropTypes\.number\.isRequired/g, 'number')
-    .replace(/: PropTypes\.number\.isRequired/g, '?: number')
-    .replace(', { PropTypes }', '')
-    .replace('const propTypes', 'type Props')
-    .replace(/.+propTypes \= propTypes;\n/, '')
+  filenames.forEach(filename => {
+    const file = readFileSync(filename, 'utf8');
 
-  writeFileSync(
-    filename,
-    `/* @flow */\n${changedFile}`
-  );
+    if (file.includes('@flow')) {
+      return;
+    }
+
+    const changedFile = file
+      .replace(/PropTypes\.object\.isRequired/g, 'Object')
+      .replace(/: PropTypes\.object\.isRequired/g, '?: Object')
+      .replace(/PropTypes\.string\.isRequired/g, 'string')
+      .replace(/: PropTypes\.string\.isRequired/g, '?: string')
+      .replace(/PropTypes\.bool\.isRequired/g, 'boolean')
+      .replace(/: PropTypes\.bool\.isRequired/g, '?: boolean')
+      .replace(/PropTypes\.number\.isRequired/g, 'number')
+      .replace(/: PropTypes\.number\.isRequired/g, '?: number')
+      .replace(', { PropTypes }', '')
+      .replace('const propTypes', 'type Props')
+      .replace(/.+propTypes \= propTypes;\n/, '')
+
+    writeFileSync(
+      filename,
+      `/* @flow */\n${changedFile}`
+    );
+  });
 });
+
